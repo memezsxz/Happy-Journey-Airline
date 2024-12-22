7185113ec963ccbc9443a2b00dc884a0b912c02e
the0xahmed <202203193@student.polytechnic.bh>
2024-12-22 03:29:17 +0000

    [Comments]

--- HappyJourneyAirline/Lib/AuthService.cs
+++ HappyJourneyAirline/Lib/AuthService.cs
@@ -8,18 +8,18 @@ namespace HappyJourneyAirline.Lib
         // Store the UserId in a text file
         public static void StoreUserId(long userId)
         {
-            string filePath = "session.txt";
+            string filePath = "session.txt"; // file that contains the current session
             File.WriteAllText(filePath, userId.ToString());  // Store the user ID in a text file
         }
 
         // Check if the user is logged in by verifying if the user ID exists and is greater than 0
         public static bool IsUserLoggedIn()
         {
-            string filePath = "session.txt";
-            if (File.Exists(filePath))
+            string filePath = "session.txt"; // file that contain current session
+            if (File.Exists(filePath)) // check if the session file exist
             {
-                string storedUserId = File.ReadAllText(filePath);
-                return int.TryParse(storedUserId, out int userId) && userId > 0;
+                string storedUserId = File.ReadAllText(filePath); // read the session file content
+                return int.TryParse(storedUserId, out int userId) && userId > 0; // if there is a valid session return true
             }
             return false;  // User is not logged in if no file exists or user ID is invalid
         }
@@ -27,11 +27,11 @@ namespace HappyJourneyAirline.Lib
         // Get the current logged-in user's ID
         public static long GetCurrentUserId()
         {
-            string filePath = "session.txt";
+            string filePath = "session.txt"; // session file name
             if (File.Exists(filePath))
             {
-                string storedUserId = File.ReadAllText(filePath);
-                if (long.TryParse(storedUserId, out long userId) && userId > 0)
+                string storedUserId = File.ReadAllText(filePath); // read sesssion file
+                if (long.TryParse(storedUserId, out long userId) && userId > 0) // check if there is correct logged in user
                 {
                     return userId;  // Return the current logged-in user ID
                 }
@@ -42,8 +42,8 @@ namespace HappyJourneyAirline.Lib
         // Remove the current logged-in user by deleting the user ID file
         public static void LogoutCurrentUser()
         {
-            string filePath = "session.txt";
-            if (File.Exists(filePath))
+            string filePath = "session.txt"; // the name of the session file
+            if (File.Exists(filePath)) // check if the session file exists
             {
                 File.Delete(filePath);  // Remove the user ID file to log out the user
             }
